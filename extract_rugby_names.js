5bde7acab3211d7988c904ae4ca65905e6d2958a
watersb1323 <watersb1323@gmail.com>
2017-02-03 00:17:46 +0000

    Final (without webpage locating)
    Can currently scrape lineups for six nations matches with this script.
    However, need to manually navigate to those webpages. Must implement
    this feature in next iteration.

--- extract_rugby_names.js
+++ extract_rugby_names.js
@@ -1,35 +1,56 @@
-// Create downloadable link
-var A = [['n','sqrt(n)']];  // initialize array of rows with header row as 1st item
-      
-for(var j=1;j<10;++j){ A.push([j, Math.sqrt(j)]) }
- 
-var csvRows = [];
-for(var i=0,l=A.length; i<l; ++i){
-        csvRows.push(A[i].join(','));   // unquoted CSV row
+//---------------------------------------------------------//
+//
+//
+//
+//---------------------------------------------------------//
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
-var csvString = csvRows.join('\n');
+
+// Define variables
+var firstTeamNames = [];
+var secondTeamNames = [];
+var csvRows = [];
+var teams = [];
+
+// Obtain team names
+teams = document.querySelector('.matchteams').getElementsByClassName('teams');
+var team1 = teams[0].innerText.toLowerCase();
+var team2 = teams[1].innerText.toLowerCase();
+
+// Obtain first team player names
+firstTeamNames = document.getElementsByClassName('namea');
+objFirstLength = firstTeamNames.length;
+
+// Obtain second team player names
+secondTeamNames = document.getElementsByClassName('nameb');
+objSecondLength = secondTeamNames.length;
+
+// Get positions
+positions = document.getElementsByClassName('pos');
+
+// Add column headers
+csvRows.push(capitalizeFirstLetter(team1) + ',' + capitalizeFirstLetter(team2) + ', Position');  
+
+// Get player names
+for(var i=0;i<objFirstLength;i++) {
+	csvRows.push(firstTeamNames[i].innerText + ',' + secondTeamNames[i].innerText + ',' + positions[i].innerText);  
+}
+
+var csvString = "data:text/csv;charset=utf-8,%EF%BB%BF\n" + csvRows.join('\n');
  
-var a = document.createElement('a');
-a.href     = 'data:attachment/csv,' + csvString;
-a.target   ='_blank';
-a.download = 'myFile.csv,' + encodeURIComponent(csvString); ;
-a.innerHTML = "Click me to download the file.";
-document.body.appendChild(a);
+// var a = document.createElement('a');
+// a.href     = 'data:attachment/csv,' + csvString;
+// a.target   ='_blank';
+// a.download = team1 + '_' + team2 + '.csv,' + encodeURIComponent(csvString);
+// a.innerHTML = "Click me to download the file.";
+// document.body.appendChild(a);
 
+var encodedUri = encodeURI(csvString);
+var link = document.createElement("a");
+link.setAttribute("href", encodedUri);
+link.setAttribute("download", team1 + '_' + team2 + ".csv");
+document.body.appendChild(link); // Required for FF
 
-// // Obtain first team names
-// firstTeamNames = document.getElementsByClassName('namea');
-// positions = document.getElementsByClassName('pos');
-// objLength = firstTeamNames.length;
-
-// for (var i=0; i < objLength; i++) {
-// 	console.log(firstTeamNames[i].innerText, positions[i].innerText);
-// }
-
-// // Obtain second team names
-// firstTeamNames = document.getElementsByClassName('namea');
-// objLength = firstTeamNames.length;
-
-// for (var i=0; i < objLength; i++) {
-// 	console.log(firstTeamNames[i].innerText, positions[i].innerText);
-// }
+link.click(); // This will download the data file named "my_data.csv".
